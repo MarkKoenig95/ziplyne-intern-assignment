4923ad9a431548c30ffb5b62efe9b1395143846b
“Mark <markkoenig95@gmail.com>
2020-05-08 17:56:30 +0800

    Main Page setup

---
+++ ziplyne-intern-assignment_mark-koenig/src/pages/MainPage/MainPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Header from "../../components/Header";
+
+import "./MainPage.css";
+
+export default function MainPage() {
+  return (
+    <div className="main-page">
+      <img
+        className="background-image"
+        src="../../images/ZL-Sample Desk.png"
+        alt=""
+      />
+      <Header />
+      <footer>
+        <p>Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</p>
+        <img src="../../images/BottomMask.png" alt="" />
+      </footer>
+    </div>
+  );
+}
