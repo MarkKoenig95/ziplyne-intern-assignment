3814d9f3014e205948992f5709169519df330af1
“Mark <markkoenig95@gmail.com>
2020-05-09 22:18:13 +0800

    Adjusted to avoid using relative path in css file

--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -5,7 +5,7 @@ import "./MainPage.css";
 
 export default function MainPage() {
   return (
-    <div className="main-page">
+    <div className="main-page" style={{backgroundImage: 'url(./images/ZL-Sample-Desk.png)'}}>
       <Header />
       <footer>
         <p>Lorem Ipsum Dolor Lorem Ipsum Dolor Amet.</p>
