e6b49cf29c4a46e5ca15b1dbe52654588cc68bdd
“Mark <markkoenig95@gmail.com>
2020-05-09 16:08:58 +0800

    Updates

---
+++ precache-manifest.06ff3c5f078dbb199beb7dd83d542e5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f04489dd1f1c2c5767cc67f0866271bf",
+    "url": "/ziplyne-intern-assignment/index.html"
+  },
+  {
+    "revision": "6dcc6d08e6c31cca251d",
+    "url": "/ziplyne-intern-assignment/static/css/main.d9c1958d.chunk.css"
+  },
+  {
+    "revision": "47cbdc56cd0ab04cf5f7",
+    "url": "/ziplyne-intern-assignment/static/js/2.2fee9bb7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ziplyne-intern-assignment/static/js/2.2fee9bb7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6dcc6d08e6c31cca251d",
+    "url": "/ziplyne-intern-assignment/static/js/main.b4c5c99f.chunk.js"
+  },
+  {
+    "revision": "1269c4c6539bf6ef1292",
+    "url": "/ziplyne-intern-assignment/static/js/runtime-main.93d4be20.js"
+  }
+]);
